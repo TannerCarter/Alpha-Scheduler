de07666ed658feccc93a45d19d4ed873d6d549f5
Tanner C <tannerc.bootcamp@gmail.com>
2021-05-23 23:59:15 -0700

    Schedule Input is now in locale storage on save

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,14 @@ var currentHour = moment().format("H");
 // Date and Time, .lead - #currentDay
 $("#currentDay").text(currentDate);
 
-console.log("Current Hour?", currentHour); //Ensure moment gave current/ correct hour check
+console.log("Current Hour?", currentHour); //Hour check
 
+//Load scheme
 $(document).ready(function () {
   colorTime();
+  scheduleText();
 });
-
+//Function to set color according to current time.
 function colorTime() {
   $(".description").each(function () {
     var perSchedule = parseInt($(this).attr("id"));
@@ -28,4 +30,63 @@ function colorTime() {
       $(this).addClass("present");
     }
   });
+}
+
+//Save button variables to use
+var descriptionTime;
+var descriptionText;
+
+//Save button functionality
+$(".saveBtn").click(function () {
+  descriptionTime = $(this).siblings(".hour").text();
+  console.log("Schedule Time:", descriptionTime);
+  descriptionText = $(this).siblings(".description").val();
+  console.log("Description:", descriptionText);
+  localStorage.setItem(descriptionTime, JSON.stringify(descriptionText));
+
+  colorTime();
+  scheduleText();
+});
+
+//Save Description
+function scheduleText() {
+  var descriptionText9 = JSON.parse(localStorage.getItem("9 A.M."));
+  $("#9").val("");
+  $("#9").val(descriptionText9);
+
+  var descriptionText10 = JSON.parse(localStorage.getItem("10 A.M."));
+  $("#10").val("");
+  $("#10").val(descriptionText10);
+
+  var descriptionText11 = JSON.parse(localStorage.getItem("11 A.M."));
+  $("#11").val("");
+  $("#11").val(descriptionText11);
+
+  var descriptionText12 = JSON.parse(localStorage.getItem("12 P.M."));
+  $("#12").val("");
+  $("#12").val(descriptionText12);
+
+  var descriptionText1 = JSON.parse(localStorage.getItem("1 P.M."));
+  $("#13").val("");
+  $("#13").val(descriptionText1);
+
+  var descriptionText2 = JSON.parse(localStorage.getItem("2 P.M."));
+  $("#14").val("");
+  $("#14").val(descriptionText2);
+
+  var descriptionText3 = JSON.parse(localStorage.getItem("3 P.M."));
+  $("#15").val("");
+  $("#15").val(descriptionText3);
+
+  var descriptionText4 = JSON.parse(localStorage.getItem("4 P.M."));
+  $("#16").val("");
+  $("#16").val(descriptionText4);
+
+  var descriptionText5 = JSON.parse(localStorage.getItem("5 P.M."));
+  $("#17").val("");
+  $("#17").val(descriptionText5);
+
+  console.log(
+    "MUST delete and save ea. description added+saved or it will persist."
+  );
 }
