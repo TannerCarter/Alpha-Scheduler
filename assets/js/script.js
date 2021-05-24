0006ec8b0e9da04a891c09c88f540cb3a487b6be
Tanner C <tannerc.bootcamp@gmail.com>
2021-05-23 23:07:01 -0700

    Added time to header, color coded times on schedule

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,31 @@
-var currentDate = moment().format("dddd MMM Do YYYY");
-
-// Update the date & time in the header
+var currentDate = moment().format("dddd, MMM Do YYYY");
+var currentHour = moment().format("H");
+// Date and Time, .lead - #currentDay
 $("#currentDay").text(currentDate);
+
+console.log("Current Hour?", currentHour); //Ensure moment gave current/ correct hour check
+
+$(document).ready(function () {
+  colorTime();
+});
+
+function colorTime() {
+  $(".description").each(function () {
+    var perSchedule = parseInt($(this).attr("id"));
+    console.log("Schedule Times", perSchedule);
+
+    if (currentHour > perSchedule) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    } else if (currentHour < perSchedule) {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    } else {
+      $(this).removeClass("future");
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    }
+  });
+}
